22c3b2253e872e3f107fa00e345dc6155f38042f
fitriaarahmawati <145914527+fitriaarahmawati@users.noreply.github.com>
2025-07-28 22:37:25 +0700

    Update streamlit_forecast_app.py

--- streamlit_forecast_app.py
+++ streamlit_forecast_app.py
@@ -38,7 +38,7 @@ st.markdown("""
         div.stButton > button:hover {
             background-color: #dcb991;
             color: white;
-            outline:none;
+            border:none;
         }
     
         /* Klik: hilangkan outline saat tombol difokuskan */
